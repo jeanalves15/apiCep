e849e66fbd9707677a9029ea7b7d558bbafd718b
jeanalves15 <jeanstella05@gmail.com>
2025-05-03 15:53:27 -0300

    Add data Anotation

--- apiCep/Controllers/EnderecoController.cs
+++ apiCep/Controllers/EnderecoController.cs
@@ -11,6 +11,10 @@ namespace apiCep.Controllers
         public readonly IEnderecoServices _enderecoServices = enderecoServices;
 
         [HttpGet("buscar/{cep}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> BuscarEndereço([FromRoute] string cep)
         {
             var response = await _enderecoServices.BuscarEndereco(cep);
